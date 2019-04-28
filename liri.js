6c9757163385a7906bcb674f838d7591396812b0
JonCurto <joncurto@gmail.com>
2019-04-27 20:43:09 -0400

    work done

--- liri.js
+++ liri.js
@@ -1,20 +1,18 @@
 var dotenv = require("dotenv").config();
 var axios = require("axios")
+var Spotify = require("node-spotify-api")
+var keys = require("./keys.js");
 
 var prog = process.argv[2]
-
-//var spotify = new Spotify(keys.spotify);
-
-
-
-//Movies! -- Command node liri.js movie-this '<movie name here>'
-
+var spotify = new Spotify(keys.spotify);
 
 
 if(prog == "movie-this") {
-    var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
     var movieName = process.argv[3]
+    var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
+    
     axios.get(movieUrl).then(function(response){
+        
         console.log("")
         console.log(response.data.Title)
         console.log("")
@@ -31,17 +29,30 @@ if(prog == "movie-this") {
 }
 //Bands!  (Name, Venue, Date)  node liri.js concert-this <artist/band name here>
 
-if(prog== "concert-this"){
+else if(prog== "concert-this"){
     var bandName = process.argv[3]
     var bandUrl = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp"
     axios.get(bandUrl).then(function(response){
-    for (i=0; i<=response.length; i++){
-        console.log(response[i].datetime)
-        console.log(response[i].)
-    }
-
         
+    for (i=0; i<response.data.length; i++){
+        console.log(response.data[i].datetime)
+        console.log("Venue:  " + response.data[i].venue.name)
+        console.log("Location:  " + response.data[i].venue.city + ", " + response.data[i].venue.country)
+        console.log("--------------------------")
+    }
     })
+}
 
+//Artist(s), The song's name, A preview link of the song from Spotify, The album that the song is from
 
+else if(prog == "spotify-this-song") {
+    var songName = process.argv[3]
+    spotify.search({ type: 'track', query: songName }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+    
+      console.log(data.tracks.items); 
+      
+    })
 }
